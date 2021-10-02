6552917eef398dbca22c27338e63b271b948a7fb
netleave <michael1@netvigator.com>
2021-10-03 05:23:12 +0800

    Create klookredeem_mary_loop.sh

---
+++ klookredeem_mary_loop.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+while true;
+do
+    M = $(date + % M)
+    S = $(date + % S)
+    if [$S - gt 58] || [$S - lt 1]
+    then
+        if ([$M - gt 58] && !([$S - eq 0])) || ([$M - eq 0] && [$S - eq 0])
+        then
+            (date + % d. % m. % y - % H: % M: % S); ('/home/maryhung2023/klookredeem_mary.sh' &); (sleep .04);
+        else
+            (date + % d. % m. % y - % H: % M: % S); ('/home/maryhung2023/klookredeem_mary.sh');
+        fi
+    fi
+done
