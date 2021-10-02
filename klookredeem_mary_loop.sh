1d79df0d7abc061e77c2f6f2c898b78275620255
netleave <michael1@netvigator.com>
2021-10-03 05:32:07 +0800

    Update klookredeem_mary_loop.sh

--- klookredeem_mary_loop.sh
+++ klookredeem_mary_loop.sh
@@ -2,15 +2,15 @@
 
 while true;
 do
-    M = $(date + % M)
-    S = $(date + % S)
-    if [$S - gt 58] || [$S - lt 1]
+    M = $(date +%M)
+    S = $(date +%S)
+    if [$S -gt 58] || [$S -lt 1]
     then
-        if ([$M - gt 58] && !([$S - eq 0])) || ([$M - eq 0] && [$S - eq 0])
+        if ([$M -gt 58] && !([$S -eq 0])) || ([$M -eq 0] && [$S -eq 0])
         then
-            (date + % d. % m. % y - % H: % M: % S); ('/home/maryhung2023/klookredeem_mary.sh' &); (sleep .04);
+            (date +%d.%m.%y - %H:%M:%S); ('/home/maryhung2023/klookredeem_mary.sh' &); (sleep .04);
         else
-            (date + % d. % m. % y - % H: % M: % S); ('/home/maryhung2023/klookredeem_mary.sh');
+            (date +%d.%m.%y - %H:%M:%S); ('/home/maryhung2023/klookredeem_mary.sh');
         fi
     fi
 done
